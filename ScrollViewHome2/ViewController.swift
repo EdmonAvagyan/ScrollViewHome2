416c7f0e9d20d62d87efccb729c01c200361fe3d
Edmon Avagyan <edmonavagyan@Edmons-MacBook-Pro.local>
2023-02-20 17:15:53 +0400

    change file

--- ScrollViewHome2/ViewController.swift
+++ ScrollViewHome2/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Hello")
 //        setupScrollView()
         // Do any additional setup after loading the view.
     }
